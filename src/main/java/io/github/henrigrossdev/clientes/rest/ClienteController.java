09b06ab91a1da4ecf00d0c92cd20be89cf941621
Henrique Grosscklauss De Souza Costa <henrique.costa@portaldedocumentos.com.br>
2022-04-08 14:14:13 -0300

    Exception Handler para ResponseStatusException

--- src/main/java/io/github/henrigrossdev/clientes/rest/ClienteController.java
+++ src/main/java/io/github/henrigrossdev/clientes/rest/ClienteController.java
@@ -29,7 +29,7 @@ public class ClienteController {
     @GetMapping("{id}")
     public Cliente acharPorId(@PathVariable Integer id){
         return repository.findById(id)
-                         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
     @DeleteMapping("{id}")
@@ -40,7 +40,7 @@ public class ClienteController {
                       repository.delete(cliente);
                       return Void.TYPE;
                   })
-                  .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                  .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
     @PutMapping("{id}")
@@ -52,6 +52,6 @@ public class ClienteController {
                       return repository.save(clienteAtualizado);
 
                   })
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 }
